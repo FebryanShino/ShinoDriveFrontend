50ba507ea0a9f9f34152599b3d0dc4d8458d6704
FebryanShino <venty779@gmail.com>
2025-05-13 20:54:40 +0700

    feat(): add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import MainLayout from "./layouts/MainLayout";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/:id" element={<Homepage />} />
-      </Routes>
-    </MainLayout>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/:id" element={<Homepage />} />
+        </Routes>
+      </MainLayout>
+    </ThemeProvider>
   );
 }
 
