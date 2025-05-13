eed0c0fe5423498085fa14529a698f2ef0b247e4
FebryanShino <venty779@gmail.com>
2025-05-13 20:57:56 +0700

    feat(): add feature to fetch for parent folders

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,16 +1,12 @@
 import AddFileButton from "@/components/AddFileButton";
+import AppSidebar from "@/components/AppSidebar";
 import ResponsiveGridWrapper from "@/components/ResponsiveGridWrapper";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { Input } from "@/components/ui/input";
-import { callAPI } from "@/config/api";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { API_URL, callAPI } from "@/config/api";
 import type { FileItem } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { SearchIcon } from "lucide-react";
@@ -30,12 +26,26 @@ export default function Homepage() {
   const { id } = useParams();
   const { data, refetch } = useQuery<FileItem>({
     queryKey: ["items", id],
-    queryFn: fetchRootFolder,
+    queryFn: fetchFolder,
   });
 
-  function fetchRootFolder() {
+  const { data: parentFiles, refetch: refetchParentFiles } = useQuery<{
+    items: FileItem[];
+  }>({
+    queryKey: ["parents", data?.parent_id],
+    queryFn: fetchParentFolder,
+  });
+
+  function fetchParentFolder() {
+    return callAPI<{ items: FileItem[] }>({
+      url: `${API_URL}/parent/${data?.parent_id ?? "root"}`,
+      method: "GET",
+    });
+  }
+
+  function fetchFolder() {
     return callAPI<FileItem>({
-      url: `http://127.0.0.1:5000/${id ?? "root"}?search=${search}`,
+      url: `${API_URL}/${id ?? "root"}?search=${search}`,
       method: "GET",
     });
   }
@@ -46,63 +56,81 @@ export default function Homepage() {
   }
 
   return (
-    <div className="w-full h-[90%] flex flex-col gap-4">
-      <div className="flex w-full h-10 justify-between">
-        <form onSubmit={handleSearchSubmit} className="flex w-full gap-2">
-          <Input
-            className="rounded-full w-[50%]"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <Button size="icon" className="rounded-full">
-            <SearchIcon />
-          </Button>
-        </form>
-        <AddFileButton
-          parentFolderId={id as string}
-          onSubmitFinished={() => refetch()}
-        />
-      </div>
-      <div className="flex"></div>
-      <div className="w-full h-full bg-gray-100 rounded-2xl p-3 flex flex-col gap-10 overflow-y-auto">
-        <ResponsiveGridWrapper minSize="15rem">
-          {data?.children &&
-            data.children
-              .filter((item) => item.type === "folder")
-              .map((item) => (
-                <Link to={`/${item.id}`}>
-                  <Card className="w-full">
-                    <CardHeader>
-                      <CardTitle>{item.name}</CardTitle>
-                    </CardHeader>
-                  </Card>
-                </Link>
-              ))}
-        </ResponsiveGridWrapper>
-        <ResponsiveGridWrapper minSize="15rem">
-          {data?.children &&
-            data.children
-              .filter((item) => item.type === "file")
-              .map((item) => (
-                <Link to={`http://127.0.0.1:5000/file/${item.id}`}>
-                  <Card className="w-full aspect-[4/3]">
-                    <CardHeader>
-                      <CardTitle className="truncate">{item.name}</CardTitle>
-                    </CardHeader>
-                    <CardContent className="px-3 h-full">
-                      <div
-                        className=" w-full h-full rounded flex justify-center items-center"
-                        style={{
-                          backgroundColor: `rgb(${hashToRGB(item.extension?.replace(".", "") as string).join(",")})`,
-                        }}
-                      >
-                        <h1 className="text-5xl">{item.extension}</h1>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </Link>
-              ))}
-        </ResponsiveGridWrapper>
-      </div>
-    </div>
+    <SidebarProvider className="w-full h-[100svh]">
+      <AppSidebar
+        items={parentFiles?.items}
+        activeItemId={data?.id as string}
+      />
+      <main className="w-full h-[100dvh] p-3">
+        <div className="py-4">
+          <SidebarTrigger size="lg" />
+        </div>
+        <div className="w-full h-[90%] flex flex-col gap-4">
+          <div className="flex w-full h-10 justify-between">
+            <form onSubmit={handleSearchSubmit} className="flex w-full gap-2">
+              <Input
+                className="rounded-full w-[50%]"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button size="icon" className="rounded-full">
+                <SearchIcon />
+              </Button>
+            </form>
+            <AddFileButton
+              filesOnTheSameDir={data?.children as FileItem[]}
+              parentFolderId={id as string}
+              onSubmitFinished={() => {
+                refetch();
+                refetchParentFiles();
+              }}
+            />
+          </div>
+          <div className="flex"></div>
+          <div className="w-full h-full bg-secondary rounded-2xl p-3 flex flex-col gap-10 overflow-y-auto">
+            <ResponsiveGridWrapper minSize="15rem">
+              {data?.children &&
+                data.children
+                  .filter((item) => item.type === "folder")
+                  .map((item) => (
+                    <Link to={`/${item.id}`} key={item.id}>
+                      <Card className="w-full">
+                        <CardHeader>
+                          <CardTitle>{item.name}</CardTitle>
+                        </CardHeader>
+                      </Card>
+                    </Link>
+                  ))}
+            </ResponsiveGridWrapper>
+            <ResponsiveGridWrapper minSize="15rem">
+              {data?.children &&
+                data.children
+                  .filter((item) => item.type === "file")
+                  .map((item) => (
+                    <Link to={`${API_URL}/file/${item.id}`} key={item.id}>
+                      <Card className="w-full aspect-[4/3]">
+                        <CardHeader>
+                          <CardTitle className="truncate">
+                            {item.name}
+                            {item.extension}
+                          </CardTitle>
+                        </CardHeader>
+                        <CardContent className="px-3 h-full">
+                          <div
+                            className=" w-full h-full rounded flex justify-center items-center"
+                            style={{
+                              backgroundColor: `rgb(${hashToRGB(item.extension?.replace(".", "") as string).join(",")})`,
+                            }}
+                          >
+                            <h1 className="text-5xl">{item.extension}</h1>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    </Link>
+                  ))}
+            </ResponsiveGridWrapper>
+          </div>
+        </div>
+      </main>
+    </SidebarProvider>
   );
 }
