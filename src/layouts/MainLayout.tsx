1e8613268ffaca7086c0363a65c96c036b3a8d62
FebryanShino <venty779@gmail.com>
2025-05-13 20:57:27 +0700

    refactor(): main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,3 @@
-import AppSidebar from "@/components/AppSidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React from "react";
 
 export default function MainLayout({
@@ -7,15 +5,5 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider className="w-full h-[100svh]">
-      <AppSidebar />
-      <main className="w-full h-[100dvh] p-3">
-        <div className="py-4">
-          <SidebarTrigger size="lg" />
-        </div>
-        {children}
-      </main>
-    </SidebarProvider>
-  );
+  return <>{children}</>;
 }
