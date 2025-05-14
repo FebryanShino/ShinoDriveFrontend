db95edf1f8279049b21743f6d4b59382771c02f0
FebryanShino <venty779@gmail.com>
2025-05-14 11:37:42 +0700

    feat(): register all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import MainLayout from "./layouts/MainLayout";
 import { ThemeProvider } from "./components/theme-provider";
+import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useState } from "react";
+import type { User } from "./types";
+import RegisterPage from "./pages/RegisterPage";
+import GuestRoute from "./components/GuestRoute";
 
 function App() {
+  const [user, setUser] = useState<User>();
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/:id" element={<Homepage />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        <Route
+          path="/register"
+          element={
+            <GuestRoute>
+              <RegisterPage />{" "}
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <GuestRoute>
+              <LoginPage />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute
+              onCheckSuccess={(authorizedUser) => setUser(authorizedUser)}
+            >
+              <Homepage user={user as User} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/:id"
+          element={
+            <ProtectedRoute
+              onCheckSuccess={(authorizedUser) => setUser(authorizedUser)}
+            >
+              <Homepage user={user as User} />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </ThemeProvider>
   );
 }
