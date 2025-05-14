803da8c599b08b3ba4db0e04624d393c7b0c076a
FebryanShino <venty779@gmail.com>
2025-05-14 11:37:29 +0700

    feat(): add access token to queries and created file breadcrumb

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,19 +1,28 @@
 import AddFileButton from "@/components/AddFileButton";
 import AppSidebar from "@/components/AppSidebar";
 import ResponsiveGridWrapper from "@/components/ResponsiveGridWrapper";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { Input } from "@/components/ui/input";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { API_URL, callAPI } from "@/config/api";
-import type { FileItem } from "@/types";
+import { getAccessToken } from "@/config/api/accessToken";
+import type { FileItem, User } from "@/types";
 import { useQuery } from "@tanstack/react-query";
-import { SearchIcon } from "lucide-react";
+import { FolderIcon, HomeIcon, SearchIcon } from "lucide-react";
 import { useState, type FormEvent } from "react";
 import { Link, useParams } from "react-router-dom";
 
-export default function Homepage() {
+export default function Homepage({ user }: { user: User }) {
   const [search, setSearch] = useState("");
   function hashToRGB(str: string) {
     const rgb = [];
@@ -40,6 +49,7 @@ export default function Homepage() {
     return callAPI<{ items: FileItem[] }>({
       url: `${API_URL}/parent/${data?.parent_id ?? "root"}`,
       method: "GET",
+      authToken: getAccessToken() as string,
     });
   }
 
@@ -47,17 +57,20 @@ export default function Homepage() {
     return callAPI<FileItem>({
       url: `${API_URL}/${id ?? "root"}?search=${search}`,
       method: "GET",
+      authToken: getAccessToken() as string,
     });
   }
 
   function handleSearchSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    console.log(data);
     refetch();
   }
 
   return (
     <SidebarProvider className="w-full h-[100svh]">
       <AppSidebar
+        user={user}
         items={parentFiles?.items}
         activeItemId={data?.id as string}
       />
@@ -66,6 +79,43 @@ export default function Homepage() {
           <SidebarTrigger size="lg" />
         </div>
         <div className="w-full h-[90%] flex flex-col gap-4">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild className="flex items-center gap-2">
+                  <Link to="/">
+                    <HomeIcon size={16} />
+                    Home
+                  </Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              {data?.parent && data?.parent.parent_id && (
+                <>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink>...</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink asChild className="flex items-center gap-2">
+                      <Link to={"/" + data.parent.id}>
+                        <FolderIcon size={16} />
+                        {data?.parent?.name}
+                      </Link>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                </>
+              )}
+              <BreadcrumbItem>
+                <BreadcrumbPage className="flex items-center gap-2">
+                  <FolderIcon size={16} />
+                  {data?.name}
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+
           <div className="flex w-full h-10 justify-between">
             <form onSubmit={handleSearchSubmit} className="flex w-full gap-2">
               <Input
