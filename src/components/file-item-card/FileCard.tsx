1a8c6bb8556f6ea7048e1061a9dc19e31e020567
FebryanShino <venty779@gmail.com>
2026-06-22 19:35:30 +0700

    feat: created file card

---
+++ src/components/file-item-card/FileCard.tsx
@@ -0,0 +1,76 @@
+import { API_URL } from "@/config/api";
+import { IMAGE_TYPES } from "@/constants";
+import { cn } from "@/lib/utils";
+import type { FileItem } from "@/types";
+import { hashToRGB } from "@/utils/misc";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import type { ManageFileItemPopoverHandle } from "../ManageFileItemPopover";
+import ManageFileItemPopover from "../ManageFileItemPopover";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+
+interface FileCardInterface {
+  fileItem: FileItem;
+  isActive?: boolean;
+  isSelected?: boolean;
+  multipleSelectMode: boolean;
+  onClick: (isSelected: boolean) => void;
+}
+
+export default function FileCard(props: FileCardInterface) {
+  const navigate = useNavigate();
+
+  const manageFileItemPopoverRef = useRef<ManageFileItemPopoverHandle>(null);
+  return (
+    <Card
+      onContextMenu={(e) => {
+        e.preventDefault();
+        manageFileItemPopoverRef.current?.open();
+      }}
+      className={cn(
+        "w-full aspect-[4/3] cursor-pointer",
+        props.isSelected
+          ? props.isActive
+            ? "bg-blue-400"
+            : "bg-blue-800"
+          : "",
+      )}
+      onClick={(event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        props.onClick(props.isSelected as boolean);
+        if (props.isActive && props.isSelected && !props.multipleSelectMode) {
+          return navigate(`/${props.fileItem.id}/detail`);
+        }
+      }}
+    >
+      <CardHeader>
+        <CardTitle className="flex justify-between items-center h-full w-full">
+          <div className="h-full flex items-center w-full">
+            <p className=" truncate w-44 h-5">
+              {props.fileItem.name + props.fileItem.extension}
+            </p>
+          </div>
+          <ManageFileItemPopover
+            ref={manageFileItemPopoverRef}
+            fileItem={props.fileItem}
+          />
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="px-3 h-full">
+        <div
+          className=" w-full h-full rounded flex justify-center items-center bg-full bg-cover bg-center"
+          style={{
+            backgroundColor: `rgb(${hashToRGB(props.fileItem.extension?.replace(".", "") as string).join(",")})`,
+            backgroundImage: `url("${API_URL}/file/${props.fileItem.id}")`,
+          }}
+        >
+          <h1 className="text-5xl">
+            {!IMAGE_TYPES.includes(String(props.fileItem.extension)) &&
+              props.fileItem.extension}
+          </h1>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
