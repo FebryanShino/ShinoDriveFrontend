38aa5e3a4081f02738bd4faa129f732f9c8edf84
FebryanShino <venty779@gmail.com>
2026-06-22 19:34:55 +0700

    feat: update types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,15 +4,33 @@ export interface FileItem {
   id: UUID;
   name: string;
   extension: string | null;
-  parent_id: string;
-  created_at: Date;
+  parent_id: string | null;
+  created_at: string;
+  updated_at: string;
+  user: User;
   type: "folder" | "file";
   children?: this[];
   parent?: this;
+  item_metadata?: ItemMetadata;
 }
 
 export interface User {
   id: UUID;
   username: string;
   email: string;
+}
+
+export interface ItemMetadata {
+  image_url: string | null;
+  content: string;
+}
+
+export interface ActivityLog {
+  type: "create" | "update" | "delete" | "move";
+  key: string | null;
+  past_items: FileItem[] | null;
+  future_items: FileItem[] | null;
+  description: string | null;
+  author: User;
+  created_at: string;
 }
